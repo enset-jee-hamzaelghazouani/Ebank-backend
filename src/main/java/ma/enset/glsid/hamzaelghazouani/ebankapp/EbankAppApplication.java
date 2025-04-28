fcd2c28588a2ac3738162adfdbc977ec7cefbbff
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-04-28 09:37:42 +0100

    feat : init project

---
+++ src/main/java/ma/enset/glsid/hamzaelghazouani/ebankapp/EbankAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid.hamzaelghazouani.ebankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/glsid/hamzaelghazouani/ebankapp/EbankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid.hamzaelghazouani.ebankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
